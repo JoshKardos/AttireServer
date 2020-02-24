cf6017878d27d6bdba530b55005be41f9caa2298
Josh Kardos <joshkardos@gmail.com>
2020-02-23 23:28:54 -0800

    checking out other usres

--- empheralkey.php
+++ empheralkey.php
@@ -4,18 +4,14 @@ try{
     
 require_once('./stripe-php/init.php');
 
-    // This assumes that $customerId has been set appropriately from session data
-    $customerId = "cus_Glx4vC2Jc3QK6O"; // get from client
-    // echo "hel";
-    // echo $_POST['api_version'];
-if (!isset($_POST['api_version']))
-{
-    exit(http_response_code(400));
-}
+    if (!isset($_POST['api_version']) || !isset($_POST['customer_id']))
+    {
+        exit(http_response_code(400));
+    }
 try {
     \Stripe\Stripe::setApiKey('sk_test_c8WpTVTZOQnrqvqWXJJLaTeQ00whht62Tb');
     $key = \Stripe\EphemeralKey::create(
-      ["customer" => $customerId],
+      ["customer" => $_POST['customer_id']],
       ["stripe_version" => $_POST['api_version']]
     );
     // echo "hello";
